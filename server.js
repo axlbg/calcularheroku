afbc0160162147757780519aace9145a390af59d
axlbg <axl_bg@hotmail.com>
2022-09-01 09:18:39 -0300

    Primer commit. Modificaciones para deploy heroku

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
 res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
-console.log('Build successful!!');
+console.log('Build successful!!!');
